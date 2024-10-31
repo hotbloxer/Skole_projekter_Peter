1d2ce3070b4cde8532f6c77ed5a7c766f8c1640f
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2024-10-31 09:13:08 +0100

    upload

--- Skole_projekter_Peter/Program.cs
+++ Skole_projekter_Peter/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Skole_projekter_Peter
+{
+
+    class RobotRally
+    {
+
+        static void Main()
+        {
+
+
+            Robot robot = new Robot();
+
+            void executeTurn (Robot robot) 
+            {
+                
+                // draw 9 cards
+                robot.drawNewHand();
+                robot.addCardToCardSlot();
+                Console.Clear();
+
+                // Game loop
+
+                for (int i = 0; i < 5; i++)
+                {
+                    robot.executeAction(robot);
+                
+                }
+
+
+            }
+
+            executeTurn(robot);
+
+        }
+    }
+}
